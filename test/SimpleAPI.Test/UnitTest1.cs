53929e68dbf6f92c8f6b19df1f340e6df22f84bf
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2020-03-24 12:52:26 +0100

    Inintial Commit

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -2,19 +2,20 @@ using System;
 using Xunit;
 using SimpleAPI.Controllers;
 
-
 namespace SimpleAPI.Test
 {
     public class UnitTest1
     {
+        
         ValuesController controller = new ValuesController();
         [Fact]
         public void GetReturnsMyName()
         {
             var returnValue = controller.Get(1);
-            Assert.Equal("Les Jackson", returnValue.Value);
-        }
+            Assert.Equal("Bjoerk", returnValue.Value);
 
+
+        }
 
 
         [Fact]
