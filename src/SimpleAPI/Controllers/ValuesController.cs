307bbd1fbbe1a74fd5d3ab44b5320741449cd4c0
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2020-03-25 14:52:44 +0100

    changed return values

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "player" };
+            return new string[] { "dotnet", "bjork" };
         }
 
         // GET api/values/5
