1707b7e1f61e8c3f27ee53435415dcde7be67750
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2020-03-25 16:08:13 +0100

    removed trigger on chriv279v

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "bjork" };
+            return new string[] { "dotnet", "bjorksen" };
         }
 
         // GET api/values/5
