2b42c9d54b8afd3f8f016f64b2c06ec4ba73a66c
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2020-03-27 08:59:13 +0100

    Trying to fail the deploy with failing test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Bjoerk";
+            return "Bjork";
         }
 
         // POST api/values
